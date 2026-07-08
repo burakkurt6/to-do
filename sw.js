6384934ddacca5dfdbbd54d6fe16f1cc346f5ed3
burakkurt02 <burakkurt2002@outlook.com>
2026-07-08 20:01:25 +0300

    mobil responsive layout

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Basit "cache-first" strateji. Alt klasörde (/to-do/) çalışacak şekilde
    tüm yollar GÖRELİ olarak ("./") register edildiği scope'a göre çözülür. */
 
-const CACHE = 'todo-cache-v1';
+const CACHE = 'todo-cache-v2';
 
 // Register scope'una göre çözülen göreli çekirdek dosyalar.
 const CORE_ASSETS = [
